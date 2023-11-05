415ef45bcbaf969bd1d0893210a80e2ff538ec3c
Martin Aguirre <tinchocaminano@gmail.com>
2023-11-05 18:50:21 -0300

    agregado los modelos y sus migraciones

--- entrega_3/models.py
+++ entrega_3/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    edad = models.IntegerField()
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    paginas = models.IntegerField()
+
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    duracion = models.IntegerField()
